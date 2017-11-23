84b911fb8ce16e5d81762787d6b48586387304c5
Ayla Tolosa-Kline <AylaMPH@gmail.com>
2017-11-23 12:33:45 +0200

    Kele Checkpoint 4 - Mentor Availability: COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,9 +13,14 @@ class Kele
   def get_me
       response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
       @user_data = JSON.parse(response.body)
-    
-    end
 
+  end
+
+    def get_mentor_availability(mentor_id)
+      response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+      @mentor_availability = JSON.parse(response.body)
+    end
+    
     private
 
       def base_api_endpoint(end_point)
