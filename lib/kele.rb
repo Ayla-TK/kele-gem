a05da7b7f4b38942e5fa666fc5a8a3729c2366e0
Ayla Tolosa-Kline <AylaMPH@gmail.com>
2017-11-23 12:53:37 +0200

    Kele Checkpoint 5 | Roadmaps & Checkpoints: COMPLETE

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,26 +1,37 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap.rb'
 
 class Kele
   include HTTParty
 
-  def initialize(email, password)
-      response = self.class.post(base_api_endpoint("sessions"), body: {"email": email, "password": password})
-      raise "Invalid email or password" if response.code == 404
-    @auth_token = response["auth_token"]
-  end
+    def initialize(email, password)
+        response = self.class.post(base_api_endpoint("sessions"), body: {"email": email, "password": password})
+        raise "Invalid email or password" if response.code == 404
+      @auth_token = response["auth_token"]
+    end
 
-  def get_me
-      response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
-      @user_data = JSON.parse(response.body)
+    def get_me
+        response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+        @user_data = JSON.parse(response.body)
 
-  end
+    end
 
     def get_mentor_availability(mentor_id)
       response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
       @mentor_availability = JSON.parse(response.body)
     end
-    
+
+    def get_roadmap(roadmap_id)
+      response = self.class.get(base_api_endpoint("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+      @roadmap = JSON.parse(response.body)
+    end
+
+    def get_checkpoint(checkpoint_id)
+      response = self.class.get(base_api_endpoint("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+      @checkpoint = JSON.parse(response.body)
+    end
+
     private
 
       def base_api_endpoint(end_point)
